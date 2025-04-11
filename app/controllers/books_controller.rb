0a5946d5b0a872c8421b520df121db9adbc3bd83
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-04-11 21:35:01 +0000

    [Add] books_controller_index_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
+    @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
@@ -15,9 +16,9 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
