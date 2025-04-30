15d281598e1da3c0fc8f7d4ee3d6b46fae7c4b25
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-04-30 13:04:56 +0000

    [Add] books_controller_success_messages_errors_messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,6 +27,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
   end
 
